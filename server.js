e79ff833478090fd50e85e32cb5767be1bc3d279
Ruth Baker <ruthmoog@gmail.com>
2019-08-25 14:54:05 +0100

    Hello world

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (request, response) {
+  response.send("Hello world!")
+});
+
 app.listen(3000, function() {
   console.log('Server is listening on port 3000 and is ready to accept requests!  HOORAY!')
 });
