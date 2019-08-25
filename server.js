b6813969baa58a31dba56358c306f0d056b95b33
Ruth Baker <ruthmoog@gmail.com>
2019-08-25 15:53:19 +0100

    Add a form with post method

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.post('/create-post', function() {
+  console.log('/create-post!')
+})
+
 app.listen(3000, function() {
   console.log('Server is listening on port 3000 and is ready to accept requests!  HOORAY!')
 });
