10185775b80946a7acad48c9b0d45b0e29afbe3b
Ruth Baker <ruthmoog@gmail.com>
2019-08-25 22:29:49 +0100

    Read and write posts.json

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var formidable = require('express-formidable');
+var fileSystem = require('fs');
+
 var app = express();
 
 app.use(express.static('public'));
 app.use(formidable());
 
+
+
+
 app.post('/create-post', function(request) {
-  console.log(request.fields)
-})
+  fileSystem.readFile(__dirname + '/data/posts.json', function (error, file) {
+    var blogPosts = JSON.parse(file);
+    var timeStamp = Date.now()
+    blogPosts[timeStamp] = request.fields.blogpost
+    fileSystem.writeFile(__dirname + '/data/posts.json', JSON.stringify(blogPosts), function (error) {
+      console.log(file.toString());
+    });
+  });
+});
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000 and is ready to accept requests!  HOORAY!')
