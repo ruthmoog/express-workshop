96ddb70eedee179e09af178426952aa061d79a29
Ruth Baker <ruthmoog@gmail.com>
2019-08-25 15:05:33 +0100

    Serve static assets files

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (request, response) {
-  response.send("Hello world!")
-});
+app.use(express.static('public'));
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000 and is ready to accept requests!  HOORAY!')
