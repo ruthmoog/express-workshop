593293529234e5aa9ca6641610cefd9d46ec5d26
Ruth Baker <ruthmoog@gmail.com>
2019-08-26 11:28:44 +0100

    Lint with Prettier

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-var express = require('express');
-var formidable = require('express-formidable');
-var fileSystem = require('fs');
+var express = require("express");
+var formidable = require("express-formidable");
+var fileSystem = require("fs");
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(formidable());
 
-app.post('/create-post', function(request) {
-  fileSystem.readFile(__dirname + '/data/posts.json', function (error, file) {
+app.post("/create-post", function(request) {
+  fileSystem.readFile(__dirname + "/data/posts.json", function(error, file) {
     var blogPosts = JSON.parse(file);
-    var timeStamp = Date.now()
+    var timeStamp = Date.now();
 
-    blogPosts[timeStamp] = request.fields.blogpost
-    fileSystem.writeFile(__dirname + '/data/posts.json', JSON.stringify(blogPosts), function (error) {
-      console.log(file.toString());
-    });
+    blogPosts[timeStamp] = request.fields.blogpost;
+    fileSystem.writeFile(
+      __dirname + "/data/posts.json",
+      JSON.stringify(blogPosts),
+      function(error) {
+        console.log(file.toString());
+      }
+    );
   });
 });
 
-app.get('/get-posts', function(request, response) {
-  response.sendFile(__dirname + '/data/posts.json');
+app.get("/get-posts", function(request, response) {
+  response.sendFile(__dirname + "/data/posts.json");
 });
 
 app.listen(3000, function() {
-  console.log('Server is listening on port 3000 and is ready to accept requests!  HOORAY!')
+  console.log(
+    "Server is listening on port 3000 and is ready to accept requests!  HOORAY!"
+  );
 });
